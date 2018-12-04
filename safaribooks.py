e735adb92f921fb0217abfbdd3e8710c96744000
Lorenzo Di Fuccia <lorenzo.difuccia@gmail.com>
2018-12-04 11:36:50 +0100

    Update safaribooks.py
    Reuse global variable

--- safaribooks.py
+++ safaribooks.py
@@ -417,7 +417,7 @@ class SafariBooks:
         return new_cred
 
     def do_login(self, email, password):
-        response = self.requests_provider("https://www.safaribooksonline.com/accounts/login")
+        response = self.requests_provider(self.LOGIN_URL)
         if response == 0:
             self.display.exit("Login: unable to reach Safari Books Online. Try again...")
 
