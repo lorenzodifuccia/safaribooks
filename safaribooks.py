4b12f14fee61dd714cfcdc30be6ffcdb2b7b320f
Lorenzo Di Fuccia <lorenzo.difuccia@gmail.com>
2019-03-03 09:21:59 -0800

    Fixes #77

--- safaribooks.py
+++ safaribooks.py
@@ -61,10 +61,10 @@ class Display:
         sys.excepthook = sys.__excepthook__
 
     def log(self, message):
-        self.logger.info(str(message, "utf-8", "replace"))
+        self.logger.info(str(message))  # TODO: "utf-8", "replace"
 
     def out(self, put):
-        sys.stdout.write("\r" + " " * self.columns + "\r" + str(put, "utf-8", "replace") + "\n")
+        sys.stdout.write("\r" + " " * self.columns + "\r" + str(put) + "\n")  # TODO: "utf-8", "replace"
 
     def info(self, message, state=False):
         self.log(message)
